28fb0f71b2277341f5be8e84e681dcaa3b8fb3d1
Raphaël Thémans <raphael.themans@outlook.be>
2024-05-05 10:38:24 +0200

    added quickget and removed steam

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -154,6 +154,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    quickemu
+    quickgui
+    netcat-gnu
     wezterm
     hplipWithPlugin
     abiword
@@ -182,8 +185,6 @@
     tmux
     unetbootin
   ];
-
-  programs.steam.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
