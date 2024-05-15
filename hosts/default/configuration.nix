9ee26aac949ecb39e464747986fc5dc0a5715c03
Raphaël Thémans <raphael.themans@outlook.be>
2024-05-15 15:16:20 +0200

    added pdf-sign

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -185,6 +185,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    pdf-sign
     spice
     quickemu
     quickgui
