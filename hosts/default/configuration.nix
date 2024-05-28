964b5cf1000de9754d361a5b14c3fd0a0534e94d
Raphaël Thémans <raphael.themans@outlook.be>
2024-05-28 21:29:34 +0200

    remove , as not used for list in nix

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -136,7 +136,7 @@ in
     enable = true;
 
     # Nvidia driver
-    videoDrivers = ["displayLink", "nvidia"];
+    videoDrivers = ["displayLink" "nvidia"];
     
     # Configure keymap in X11    
     xkb.layout = "fr";
