9c25ed03cda0f4f074214ac7763a9925d7b40e38
Raphaël Thémans <raphael.themans@outlook.be>
2024-05-31 09:40:37 +0200

    added pavucontrol

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -196,6 +196,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    pavucontrol
     unzip
     jetbrains.idea-ultimate
     maven
