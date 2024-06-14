e03ca1dd812a4b9d1f0783e9462342ce3281d7a5
rthemans <raph.the@gmail.com>
2024-06-14 15:57:59 +0200

    removed stylix from flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    stylix.url = "github:danth/stylix";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -23,7 +22,6 @@
           modules = [ 
             ./hosts/default/configuration.nix
             inputs.home-manager.nixosModules.default
-	    inputs.stylix.nixosModules.stylix
           ];
         };
       server = nixpkgs.lib.nixosSystem {
