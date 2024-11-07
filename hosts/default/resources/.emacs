2e003b768a1272f10afe717fa95ca4de6da1c5f6
rthemans <raph.the@gmail.com>
2024-11-07 10:13:35 +0100

    added emacs dotfiles

---
+++ hosts/default/resources/.emacs
@@ -0,0 +1,1 @@
+(setq display-line-numbers 'relative)
---
+++ hosts/default/resources/emacs/init.el
@@ -0,0 +1,27 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; recipes
+(fzf :repo "bling/fzf.el" :fetcher github)
+(solarized-theme :repo "bbatsov/solarized-emacs" :fetcher github)
+
+;; configuration
+
+(load-theme 'solarized-light t)
+
+(use-package fzf
+;; :bind
+;; Don't forget to set keybinds!
+  :config
+  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+        fzf/executable "fzf"
+        fzf/git-grep-args "-i --line-number %s"
+        ;; command used for `fzf-grep-*` functions
+        ;; example usage for ripgrep:
+        ;; fzf/grep-command "rg --no-heading -nH"
+        fzf/grep-command "grep -nrH"
+        ;; If nil, the fzf buffer will appear at the top of the window
+        fzf/position-bottom t
+        fzf/window-height 15))
