9f04eab45915362677783e27d24f4c6443dee8b3
rthemans <raph.the@gmail.com>
2025-12-04 14:02:26 +0100

    [hypr] other fixes from mammouth.ai

--- hosts/default/hardware-configuration.nix
+++ hosts/default/hardware-configuration.nix
@@ -73,7 +73,7 @@
   hardware.nvidia = {
 
     # Modesetting is required.
-    modesetting.enable = false;
+    modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
@@ -83,7 +83,7 @@
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    powerManagement.finegrained = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -92,7 +92,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
